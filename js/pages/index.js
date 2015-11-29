88af0fcd7ed0f5b10ab03b85573649d39add87a3
SamMcRuvie <doctorwhat35@gmail.com>
2015-11-29 16:06:02 +0000

    Add Comments, Add Style classes to CSS and apply to html
    Comments have been added to all my code to improve readability.
    Added style classes to match the design.
    These have been applied to the navigation bar.
    
    Signed-Off-By: Sam Mcruvie <doctorwhat35@gmail.com>

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,14 +1,13 @@
-/* not refined work but should be used for testing*/
 $(document).ready(function(){
 
-  
-  var innerLinks = $(".innerLinks"); // element selectopr
-  var outerLinks = $(".outerNavElement:nth-child(2)"); //element selectopr
-
-  innerLinks.hide();
+  // element selectopr
+  var innerLinks = $(".innerLinks"); 
+  var outerLinks = $(".outerNavElement:nth-child(2)"); 
+  //hide elements
+  innerLinks.hide(); 
   outerLinks.hide();
-
-  innerLinks.fadeIn("slow"); //fade in inner elements
+  //fade in elements
+  innerLinks.fadeIn("slow"); 
   outerLinks.fadeIn("slow");
   
 });
