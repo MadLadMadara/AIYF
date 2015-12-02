2324d80d8ced36cfe475f836993a0bad4b464057
SamMcRuvie <doctorwhat35@gmail.com>
2015-12-02 14:54:13 +0000

    Index page Nav bar animation and links.
    The nav bar on the index page has been fully animated. All links on the
    index page now lead to there proper location.
    
    Signed-Off-By: Sam Mcruvie <doctorwhat35@gmail.com>

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
 
   // element selectopr
+  var linkContainers = $(".navLinkElementContainer"); 
   var innerLinks = $(".innerLinks"); 
-  var outerLinks = $(".outerNavElement:nth-child(2)"); 
+  var outerLinks = $(".outerNavElement:nth-child(2)");
+
   //hide elements
   innerLinks.hide(); 
   outerLinks.hide();
+
   //fade in elements
   innerLinks.fadeIn("slow"); 
   outerLinks.fadeIn("slow");
+
+  /* 
+  on mouse over of .innerLinks change
+  the colour of the element.
+  */
+  linkContainers.hover(function(){ // on hover 
+
+  	$(this).css("background-color", "rgba(144,144,144, 0.8)");
+
+	}, function(){ // off hover
+
+		$(this).css("background-color", "rgba(144,144,144, 0.5)");
+
+	});
   
 });
