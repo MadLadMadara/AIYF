ddea17bdc76dc630b6b8bc03a3dba6a88521f218
SamMcRuvie <doctorwhat35@gmail.com>
2015-12-02 15:47:36 +0000

    change index.js hover colour and remove gallery.html
    Signed-Off-By: Sam Mcruvie <doctorwhat35@gmail.com>

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,30 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function(){ // wait until the html/css to load
 
-  // element selectopr
+  // element selectors, selecting by class
+  var dom = $("body, html");
   var linkContainers = $(".navLinkElementContainer"); 
   var innerLinks = $(".innerLinks"); 
   var outerLinks = $(".outerNavElement:nth-child(2)");
 
-  //hide elements
+  // hide elements
   innerLinks.hide(); 
   outerLinks.hide();
 
-  //fade in elements
+  // fade in elements
   innerLinks.fadeIn("slow"); 
   outerLinks.fadeIn("slow");
 
   /* 
   on mouse over of .innerLinks change
-  the colour of the element.
+  the colour of the element that the mouse
+  is hovering over.
   */
-  linkContainers.hover(function(){ // on hover 
+  	linkContainers.hover(function(){ // on hover 
 
-  	$(this).css("background-color", "rgba(144,144,144, 0.8)");
+  		$(this).css("background-color", "rgba(144,144,144, 0.8)");
 
 	}, function(){ // off hover
 
 		$(this).css("background-color", "rgba(144,144,144, 0.5)");
 
 	});
+
+  
   
 });
